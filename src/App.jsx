19a3223d16fc3ebf4b36993e63331cae2b27ea6b
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-17 22:02:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,30 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AboutUs from "./components/AboutUs";
+import ProductList from "./components/ProductList";
+import CartItem from "./components/CartItem";
+import "./App.css";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="landing">
+              <h1>Paradise Nursery</h1>
+              <AboutUs />
+              <Link to="/plants">
+                <button>Get Started</button>
+              </Link>
+            </div>
+          }
+        />
+        <Route path="/plants" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
