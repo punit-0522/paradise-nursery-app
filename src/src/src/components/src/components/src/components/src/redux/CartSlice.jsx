7aec22ab0ada28788e11a97157484afc24423fcf
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-17 22:05:16 +0530

    Update CartSlice.jsx

--- src/src/src/components/src/components/src/components/src/redux/CartSlice.jsx
+++ src/src/src/components/src/components/src/components/src/redux/CartSlice.jsx
@@ -1,1 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: { items: [] },
+  reducers: {
+    addToCart: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload.id);
+      if (item) item.quantity++;
+      else state.items.push({ ...action.payload, quantity: 1 });
+    },
+
+    increase: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      item.quantity++;
+    },
+
+    decrease: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      if (item.quantity > 1) item.quantity--;
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(i => i.id !== action.payload);
+    },
+  },
+});
+
+export const { addToCart, increase, decrease, removeItem } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
